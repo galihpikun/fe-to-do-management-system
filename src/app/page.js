37b71c37a5adf4da36306207722357fb4f8c4599
galihpikun <galihrakha003@gmail.com>
2025-01-18 15:58:00 +0700

    nambah list

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,8 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.cape banget</li>
+          <li>Save and see your changes instantly.</li>
+          <li>Gila Guru tamu susah banget</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
